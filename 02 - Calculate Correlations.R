7c438006427c8ff4673e74f390458923e811f55e
Mark Ewing <mewing@eastman.com>
2016-04-01 06:50:00 -0400

    Modified correlation calculations
    Now includes many more columns for 'wide' sets and also writes out the
    resulting correlation matrix to a csv

--- 02 - Calculate Correlations.R
+++ 02 - Calculate Correlations.R
@@ -4,22 +4,32 @@ library(dplyr) #if not installed; install.packages('dplyr')
 
 dataFiles = c("2008.csv","00-08.csv","95-08.csv")
 
+types = c("i", "i", "i", "i", "i", "i",
+  "i", "i", "c", "i", "c", "i",
+  "i", "i", "i", "i", "c", "c",
+  "i", "i", "i", "i", "c", "i",
+  "i", "i", "i", "i", "i"
+) %>% paste(collapse='')
+
 for(i in dataFiles){
   #First extract all numeric columns for which a correlation makes sense
-  tmp = readr::read_csv(i) %>% 
-    dplyr::select(ActualElapsedTime, CRSElapsedTime, AirTime, ArrDelay, DepDelay,
-                  Distance, TaxiIn, TaxiOut, CarrierDelay, WeatherDelay, NASDelay,
-                  SecurityDelay, LateAircraftDelay)
+  tmp = readr::read_csv(i,col_types = types) %>%
+    dplyr::select(which(strsplit(types,"")[[1]] == "i")) %>%
+    dplyr::select(-Year)
   #Calculate correlation matrix with pairwise deletion
-  timing_wide = system.time({correls_wide = cor(tmp,use="na.or.complete")})
-  print(correls_wide)
+  try({
+    timing_wide = system.time({correls_wide = cor(tmp,use="na.or.complete")})
+    write.csv(correls_wide,paste0("Correlations/",strsplit(i,"\\.")[[1]][1]," wide correls R.txt"))
+    })
+
   #reduce the number of columns
-  
-  tmp %<>% select(-contains("Delay"),-contains("Taxi"))
+  tmp %<>% select(ActualElapsedTime, CRSElapsedTime, AirTime, Distance)
   #Calculate correlation matrix with pairwise deletion
-  timing_narrow = system.time({correls_narrow = cor(tmp,use="na.or.complete")})
-  print(correls_narrow)
-  
+  try({
+    timing_narrow = system.time({correls_narrow = cor(tmp,use="na.or.complete")})
+    write.csv(correls_narrow,paste0("Correlations/",strsplit(i,"\\.")[[1]][1]," narrow correls R.txt"))
+  })
+
   #clean up
   readr::write_csv(dplyr::data_frame(File=i,Tool="R",NarrowTime=timing_narrow[3],WideTime=timing_wide[3],Technique="correlation"),
                    "performance measures.txt",
