a3aeb63fca098e38b98a3b2bb6d5af7b71550ce7
faizabidi <fabidi89@vt.edu>
2016-04-17 12:51:39 -0400

    Fixed regression code.

--- DecisionTrees/DecisionTreesRegression/Narrow/decision_tree_regression-narrow.py
+++ DecisionTrees/DecisionTreesRegression/Narrow/decision_tree_regression-narrow.py
@@ -4,9 +4,9 @@ from numpy import array
 from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
 from datetime import datetime
 
-sc = SparkContext ("local", "Run 1 - Decision Tree Classification Narrow - Data2008 - Single Node")
+sc = SparkContext ("local", "Run 1 - Decision Tree Regression Narrow - Data2008 - Single Node")
 
-data_file = "2008-reduced.csv"
+data_file = "2008.csv"
 raw_data = sc.textFile (data_file).cache ()
 #extract the header
 header = raw_data.first ()
@@ -18,40 +18,49 @@ def parsePoint (line):
 	line_split = line.split (",")
 	#replace NA with zeros
 	line_split = [w.replace ('NA', '0') for w in line_split]
-	#make Cancelled as binary since that's our response
-	if (line_split[21] > 0):
-		line_split[21] = 1
-	else:
-		line_split[21] = 0
-	#keep just numeric values
+	
+	#keep only the columns needed
 	"""
+	4 = DepTime
 	5 = CRSDepTime
+	6 = ArrTime
 	7 = CRSArrTime
+	11 = ActualElapsedTime
 	12 = CRSElapsedTime
+	13 = ArrTime
+	14 = ArrDelay
+	15 - DepDelay
 	18 = Distance
-	21 = Cancelled,
+	19 = TaxiIn
+	20 = TaxiOut
 	"""
-	symbolic_indexes = [5, 7, 12, 18, 21]
+
+	symbolic_indexes = [4, 5, 6, 7, 11, 12, 13, 14, 15, 18, 19, 20]
 	clean_line_split = [item for i, item in enumerate (line_split) if i in symbolic_indexes]
 	
-	#Cancelled becomes the 5th column now, and total columns in the data = 5
-	label = clean_line_split[4]
-	nonLable = clean_line_split[0:4]
+	#ArrDelay is our response
+	#ArrDelay becomes the 8th column now, and total columns in the data = 8
+	label = clean_line_split[7]
+	nonLable = clean_line_split[0:7] + clean_line_split[8:]
 	return LabeledPoint (label, nonLable)
 
 parsedData = raw_data.map (parsePoint)
+
 #divide training and test data by 70-30 rule
 (trainingData, testData) = parsedData.randomSplit([0.7, 0.3])
 
 #start timer at this point
 startTime = datetime.now()
 #build the model
-model = DecisionTree.trainRegressor (trainingData, categoricalFeaturesInfo={},
-                                        impurity='variance', maxDepth=3, maxBins=16)
+model = DecisionTree.trainRegressor (training, categoricalFeaturesInfo={},
+                                         impurity='variance', maxDepth=5, maxBins=32)
+
 #evaluate model on test instances and compute test error
-predictions = model.predict(testData.map (lambda x: x.features))
-labelsAndPredictions = testData.map (lambda lp: lp.label).zip(predictions)
-testMSE = labelsAndPredictions.map (lambda (v, p): (v - p) * (v - p)).sum() / float (testData.count())
+predictions = model.predict (test.map (lambda x: x.features))
+labelsAndPredictions = test.map (lambda lp: lp.label).zip (predictions)
+testMSE = labelsAndPredictions.map (lambda (v, p): (v - p) * (v - p)).sum() /\
+    float(testData.count())
+
 print ('Time consumed = '), (datetime.now() - startTime)
 
 print ('Test Mean Squared Error = ' + str (testMSE))
@@ -59,5 +68,5 @@ print ('Learned regression tree model:')
 print (model.toDebugString())
 
 #save and load model
-#model.save (sc, "DTR-Narrow-2008")
-#sameModel = DecisionTreeModel.load(sc, "DTR-Narrow-2008")
+model.save (sc, "DTR-Narrow-2008")
+sameModel = DecisionTreeModel.load (sc, "DTR-Narrow-2008")
--- DecisionTrees/DecisionTreesRegression/Wide/decision_tree_regression-wide.py
+++ DecisionTrees/DecisionTreesRegression/Wide/decision_tree_regression-wide.py
@@ -4,9 +4,9 @@ from numpy import array
 from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
 from datetime import datetime
 
-sc = SparkContext ("local", "Run 1 - Decision Tree Classification Wide - Data2008 - Single Node")
+sc = SparkContext ("local", "Run 1 - Decision Tree Regression Wide - Data2008 - Single Node")
 
-data_file = "2008-reduced.csv"
+data_file = "2008.csv"
 raw_data = sc.textFile (data_file).cache ()
 #extract the header
 header = raw_data.first ()
@@ -18,29 +18,34 @@ def parsePoint (line):
 	line_split = line.split (",")
 	#replace NA with zeros
 	line_split = [w.replace ('NA', '0') for w in line_split]
-	#make Cancelled as binary since that's our response
-	if (line_split[21] > 0):
-		line_split[21] = 1
-	else:
-		line_split[21] = 0
-	#keep just numeric values
-	#keep just numeric values
+	
+	#keep only the columns needed
 	"""
 	1 = Month
 	2 = DayofMonth
 	3 = DayOfWeek
+	4 = DepTime
 	5 = CRSDepTime
+	6 = ArrTime
 	7 = CRSArrTime
+	8 = UniqueCarrier - Non numeric
+	11 = ActualElapsedTime
 	12 = CRSElapsedTime
+	13 = ArrTime
+	14 = ArrDelay
+	15 - DepDelay
 	18 = Distance
-	21 = Cancelled,
+	19 = TaxiIn
+	20 = TaxiOut
 	"""
-	symbolic_indexes = [1, 2, 3, 5, 7, 12, 18, 21]
+
+	symbolic_indexes = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 18, 19, 20]
 	clean_line_split = [item for i, item in enumerate (line_split) if i in symbolic_indexes]
 	
-	#Cancelled becomes the 8th column now, and total columns in the data = 8
-	label = clean_line_split[7]
-	nonLable = clean_line_split[0:7]
+	#ArrDelay is our response
+	#ArrDelay becomes the 11th column now, and total columns in the data = 11
+	label = clean_line_split[10]
+	nonLable = clean_line_split[0:10] + clean_line_split[11:]
 	return LabeledPoint (label, nonLable)
 
 parsedData = raw_data.map (parsePoint)
@@ -50,19 +55,22 @@ parsedData = raw_data.map (parsePoint)
 #start timer at this point
 startTime = datetime.now()
 #build the model
-model = DecisionTree.trainClassifier (trainingData, numClasses=2, categoricalFeaturesInfo={},
-                                         impurity='gini', maxDepth=3, maxBins=16)
+#empty categoricalFeaturesInfo indicates all features are continuous.
+model = DecisionTree.trainRegressor (training, categoricalFeaturesInfo={},
+                                         impurity='variance', maxDepth=5, maxBins=32)
+
 #evaluate model on test instances and compute test error
-predictions = model.predict(testData.map (lambda x: x.features))
-labelsAndPredictions = testData.map (lambda lp: lp.label).zip(predictions)
-testErr = labelsAndPredictions.filter (lambda (v, p): v != p).count() / float (testData.count())
+predictions = model.predict (test.map (lambda x: x.features))
+labelsAndPredictions = test.map (lambda lp: lp.label).zip (predictions)
+testMSE = labelsAndPredictions.map (lambda (v, p): (v - p) * (v - p)).sum() /\
+    float(testData.count())
+
 print ('Time consumed = '), (datetime.now() - startTime)
 
-print ('Test Error = ' + str (testErr))
-print ('Learned classification tree model:')
+print ('Test Mean Squared Error = ' + str (testMSE))
+print ('Learned regression tree model:')
 print (model.toDebugString())
 
 #save and load model
-#model.save(sc, "DT-Wide-2008")
-#sameModel = DecisionTreeModel.load(sc, "DTC-Wide-2008")
-#sc.stop ()
+model.save (sc, "DTR-Wide-2008")
+sameModel = DecisionTreeModel.load (sc, "DTR-Wide-2008")
