186c190ce943a20e93c6e4ac0b495b342a0d1227
Sangeetha <bsangee@vt.edu>
2016-05-17 23:26:46 -0400

    Update launchjobs.sh

--- spark_vs_r_AWS/SVM/LatestSparkCode/launchjobs.sh
+++ spark_vs_r_AWS/SVM/LatestSparkCode/launchjobs.sh
@@ -3,3 +3,8 @@
 /usr/bin/time -v spark-submit --executor-cores 16 --executor-memory 20G --driver-cores 16 Narrow/00-08/SVM.py > Narrow/00-08/results_aws.txt;
 /usr/bin/time -v spark-submit --executor-cores 16 --executor-memory 20G --driver-cores 16 Narrow/95-08/SVM.py > Narrow/95-08/results_aws.txt;
 
+/usr/bin/time -v spark-submit --executor-cores 16 --executor-memory 20G --driver-cores 16 Wide/2008/SVM.py >> Wide/2008/results_aws.txt;
+/usr/bin/time -v spark-submit --executor-cores 16 --executor-memory 20G --driver-cores 16 Wide/00-08/SVM.py >> Wide/00-08/results_aws.txt;
+/usr/bin/time -v spark-submit --executor-cores 16 --executor-memory 20G --driver-cores 16 Wide/95-08/SVM.py >> Wide/95-08/results_aws.txt;
+
+
