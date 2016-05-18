cf029cf6c91d21128bf490d158f753fa8f3624ef
Sangeetha <bsangee@vt.edu>
2016-05-17 23:34:48 -0400

    Update decision_tree_classification.py

--- spark_vs_r_AWS/DecisionTreesClassification/LatestSparkCode/Wide/2008/decision_tree_classification.py
+++ spark_vs_r_AWS/DecisionTreesClassification/LatestSparkCode/Wide/2008/decision_tree_classification.py
@@ -3,9 +3,9 @@ from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
 from datetime import datetime
 
-sc = SparkContext ("local[*]", "Run 1 - Decision Tree Classification Wide - Data2008 - Single Node")
+sc = SparkContext (appName= "Run 1 - Decision Tree Classification Wide - Data2008 - AWS")
 
-data_file = "/home/faiz89/Desktop/Eastman/2008.csv"
+data_file = "s3://aws-logs-012060642840-us-west-2/elasticmapreduce/cloud_proj/2008.csv"
 raw_data = sc.textFile (data_file).cache ()
 #extract the header
 header = raw_data.first ()
